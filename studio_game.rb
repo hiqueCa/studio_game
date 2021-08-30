1426442fd53fad31062f7ad19719d9a5520a9cd7
Henrique Caltram <henrivai@hotmail.com>
2021-08-30 12:45:44 -0300

    Updated studio_game.rb file as of "Separate Source Files" lesson in Ruby course

--- studio_game.rb
+++ studio_game.rb
@@ -1,4 +1,5 @@
 # require_relative is used when the files required are stored inside the same folder as the main file
+# requiring game.rb file is enough as player.rb file is required inside game.rb file, so its required automatticaly when requiring game.rb
 require_relative 'game'
 
 player_1 = Player.new("larry")
